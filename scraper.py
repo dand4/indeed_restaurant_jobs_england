5f37c60e820761f53c1fb80368af9cdaf6e48eb6
aloviza <43601602+aloviza@users.noreply.github.com>
2019-05-19 12:41:32 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 import requests
-import bs4
 from bs4 import BeautifulSoup
+import scraperwiki
+from datetime import datetime
+import pytz
+
+# import requests
+#import bs4
+#from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
